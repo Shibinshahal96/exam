2e74a19aec966e8ee7a2d62b4d9af293fb6b1e9a
Shibinshaha-l <shibinshahal.pk@gmail.com>
2025-01-07 14:19:03 +0530

    first commit

---
+++ b) chack_number_prime.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+if num>2:
+    if num%2 == 0:
+        print("this number not prime")
+        
+    else:
+        print("this number is prime")
+        
+else:
+    print("this number is not prime")
+            
---
+++ d) Global variable and local variable.py
@@ -0,0 +1,15 @@
+# Local variable 
+def sum():
+    a = 10
+    b = 20
+    c = a + b
+    print(c)
+
+
+sum()
+
+# Global variable
+
+r= 30
+s = 40
+print(r+s)

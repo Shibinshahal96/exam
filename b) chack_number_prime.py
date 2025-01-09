db5b01b64cc783f573069dcdb050f5598610a9c3
Shibinshaha-l <shibinshahal.pk@gmail.com>
2025-01-09 16:37:38 +0530

    second commit

--- b) chack_number_prime.py
+++ b) chack_number_prime.py
@@ -1,11 +1,13 @@
 num = int(input("Enter a number: "))
+if num == 2:
+    print("this number is prime")
 if num>2:
-    if num%2 == 0:
-        print("this number not prime")
-        
+    for i in range(2,num):
+        if num%i == 0:
+            print("this number is not prime")
+            break
+            
     else:
         print("this number is prime")
-        
-else:
-    print("this number is not prime")
+            
             
